6ce9efccdcd3732c88f5480a9a18996880b976b1
Raymond Dodge <git@rayrobdod.name>
2025-07-02 14:47:53 -0400

    [gfxc] Additional frit compression tests at length boundaries

--- tools/gfxc/test/test_frit.cpp
+++ tools/gfxc/test/test_frit.cpp
@@ -19,6 +19,22 @@ void test_frit8_Zeros_32(void) {
 	}
 }
 
+void test_frit16_Zeros_30(void) {
+	const std::vector<uint8_t> input(30);
+	std::optional<std::vector<uint8_t>> result_opt = compressFrit16(input);
+
+	const std::vector<uint8_t> expected{
+		0x42, 0x1E, 0, 0,
+		0x8E, 0, 0, 0
+	};
+	if (result_opt) {
+		std::vector<uint8_t> result = *result_opt;
+		TEST_ASSERT_EQUAL_VECTOR_HEX8(expected, result);
+	} else {
+		TEST_FAIL("Compression failed");
+	}
+}
+
 void test_frit16_Zeros_32(void) {
 	const std::vector<uint8_t> input(32);
 	std::optional<std::vector<uint8_t>> result_opt = compressFrit16(input);
@@ -26,6 +42,38 @@ void test_frit16_Zeros_32(void) {
 	const std::vector<uint8_t> expected{
 		0x42, 0x20, 0, 0,
 		0x8E, 0x80, 0, 0
+	};
+	if (result_opt) {
+		std::vector<uint8_t> result = *result_opt;
+		TEST_ASSERT_EQUAL_VECTOR_HEX8(expected, result);
+	} else {
+		TEST_FAIL("Compression failed");
+	}
+}
+
+void test_frit16_Zeros_60(void) {
+	const std::vector<uint8_t> input(60);
+	std::optional<std::vector<uint8_t>> result_opt = compressFrit16(input);
+
+	const std::vector<uint8_t> expected{
+		0x42, 60, 0, 0,
+		0x8E, 0x8E, 0, 0
+	};
+	if (result_opt) {
+		std::vector<uint8_t> result = *result_opt;
+		TEST_ASSERT_EQUAL_VECTOR_HEX8(expected, result);
+	} else {
+		TEST_FAIL("Compression failed");
+	}
+}
+
+void test_frit16_Zeros_62(void) {
+	const std::vector<uint8_t> input(62);
+	std::optional<std::vector<uint8_t>> result_opt = compressFrit16(input);
+
+	const std::vector<uint8_t> expected{
+		0x42, 62, 0, 0,
+		0x8F, 0, 0, 0
 	};
 	if (result_opt) {
 		std::vector<uint8_t> result = *result_opt;
@@ -58,7 +106,10 @@ int main(int argc, char** argv) {
 	failed = 0;
 
 	RUN_TEST(test_frit8_Zeros_32);
+	RUN_TEST(test_frit16_Zeros_30);
 	RUN_TEST(test_frit16_Zeros_32);
+	RUN_TEST(test_frit16_Zeros_60);
+	RUN_TEST(test_frit16_Zeros_62);
 	RUN_TEST(test_frit8_sixteen_loop);
 
 	printf("Total: %d; Failing: %d\n", total, failed);
