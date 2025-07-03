77a54cde733c1c195c972510131c799c753ed264
Raymond Dodge <git@rayrobdod.name>
2025-07-03 01:33:00 -0400

    Implement LZ11UnCompWram

--- source/decompress/lz11.c
+++ source/decompress/lz11.c
@@ -87,3 +87,61 @@ void LZ11UnCompVram(const void* src, volatile void* dest) {
 		}
 	}
 }
+
+void LZ11UnCompWram(const void* srcV, volatile void* destV) {
+	const uint32_t* src32 = (const uint32_t*)srcV;
+	volatile uint8_t* dest = (volatile uint8_t*)destV;
+	_Static_assert(1 == sizeof(uint8_t));
+	volatile uint8_t* const dest_end = dest + (*(src32++) >> 8);
+
+	const uint8_t* src8 = (const uint8_t*)src32;
+
+	while (dest < dest_end) {
+		unsigned flags = *(src8++);
+
+		for (int i = 7; i >= 0; --i) {
+			if (dest >= dest_end)
+				break;
+
+			if (flags & (1 << i)) {
+				unsigned width;
+				unsigned distance;
+
+				switch (*src8 >> 4) {
+				case 0:
+					width = ((*src8 << 4) | ((*(src8 + 1) >> 4) & 0xF)) + 0x11;
+					distance = (((*(src8 + 1) & 0xF) << 8) | (*(src8 + 2))) + 1;
+					src8 += 3;
+					break;
+				case 1:
+					width = (((*src8 & 0xF) << 12) | (*(src8 + 1) << 4) | ((*(src8 + 2) >> 4) & 0xF)) + 0x111;
+					distance = (((*(src8 + 2) & 0xF) << 8) | (*(src8 + 3))) + 1;
+					src8 += 4;
+					break;
+				default:
+					width = (*src8 >> 4) + 1;
+					distance = (((*src8 & 0xF) << 8) | (*(src8 + 1))) + 1;
+					src8 += 2;
+					break;
+				}
+
+				volatile uint8_t* from = dest - distance;
+				for (; width > 7; width -= 8, dest += 8, from += 8) {
+					*(dest + 0) = *(from + 0);
+					*(dest + 1) = *(from + 1);
+					*(dest + 2) = *(from + 2);
+					*(dest + 3) = *(from + 3);
+					*(dest + 4) = *(from + 4);
+					*(dest + 5) = *(from + 5);
+					*(dest + 6) = *(from + 6);
+					*(dest + 7) = *(from + 7);
+				}
+				for (; width > 0; --width, ++dest, ++from) {
+					*(dest) = *(from);
+				}
+			} else {
+				*(dest++) = *(src8++);
+			}
+		}
+	}
+}
--- source/decompress/lz11.h
+++ source/decompress/lz11.h
@@ -1,1 +1,2 @@
 void LZ11UnCompVram(const void* src, volatile void* dest);
+void LZ11UnCompWram(const void* src, volatile void* dest);
