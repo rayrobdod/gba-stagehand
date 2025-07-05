0922018b77da2b2c2c0b8d17ed9b885842aa09f1
Raymond Dodge <git@rayrobdod.name>
2025-07-04 22:16:16 -0400

    Formatting and label names, and section names in assembly sources

--- source/decompress/identity.s
+++ source/decompress/identity.s
@@ -1,8 +1,6 @@
-	.cpu arm7tdmi
-	.arch armv4t
-	.section .text
+	.section .text.IdentityUnComp
 	.thumb
-	.align 2
+	.balign 2
 	.syntax unified
 	.global IdentityUnComp
 	.thumb_func
@@ -11,51 +9,51 @@ IdentityUnComp:
 	// r0 - src
 	// r1 - dest
 	// r2 - length remaining
-	ldmia r0!, {r2}
+	ldmia	r0!, {r2}
 	lsrs	r2, r2, #8
-	cmp r2, #31
-	bls  .LTEQ31
-	push {r4, r5, r6}
-.GT31:
-	ldmia r0!, {r3, r4, r5, r6}
-	stmia r1!, {r3, r4, r5, r6}
-	ldmia r0!, {r3, r4, r5, r6}
-	stmia r1!, {r3, r4, r5, r6}
-	subs r2, r2, #32
-	cmp r2, #31
-	bgt  .GT31
-	pop {r4, r5, r6}
-.LTEQ31:
+	cmp	r2, #31
+	bls	.L_32LoopEnd
+	push	{r4, r5, r6}
+.L_32LoopBegin:
+	ldmia	r0!, {r3, r4, r5, r6}
+	stmia	r1!, {r3, r4, r5, r6}
+	ldmia	r0!, {r3, r4, r5, r6}
+	stmia	r1!, {r3, r4, r5, r6}
+	subs	r2, r2, #32
+	cmp	r2, #31
+	bgt	.L_32LoopBegin
+	pop	{r4, r5, r6}
+.L_32LoopEnd:
 
 	// most common use case should be 4bpp tiles, which are guarenteed to be a multiple of 32 bytes,
 	// and maps which are likely to have a width of 32 and thus be a multiple of 32 bytes.
 	// Exit early in this common case
-	cmp r2, #0
-	bne .NEQ0_1
+	cmp	r2, #0
+	bne	1f
 	bx	lr
-.NEQ0_1:
+1:
 
-	cmp r2, #3
-	ble  .LTEQ3
-.GT3:
-	ldmia r0!, {r3}
-	stmia r1!, {r3}
-	subs r2, r2, #4
-	cmp r2, #3
-	bgt  .GT3
-.LTEQ3:
-	cmp r2, #1
-	ble  .LTEQ1
+	cmp	r2, #3
+	ble	.L_4LoopEnd
+.L_4LoopBegin:
+	ldmia	r0!, {r3}
+	stmia	r1!, {r3}
+	subs	r2, r2, #4
+	cmp	r2, #3
+	bgt	.L_4LoopBegin
+.L_4LoopEnd:
+	cmp	r2, #1
+	ble	1f
 	ldrh	r3, [r0]
 	strh	r3, [r1]
-	adds r0, r0, #2
-	adds r1, r1, #2
-	subs r2, r2, #2
-.LTEQ1:
-	cmp r2, #0
-	bne  .NEQ0
+	adds	r0, r0, #2
+	adds	r1, r1, #2
+	subs	r2, r2, #2
+1:
+	cmp	r2, #0
+	bne	1f
 	bx	lr
-.NEQ0:
+1:
 	ldrb	r3, [r0]
 	strb	r3, [r1]
 	bx	lr
--- source/gba/bios.s
+++ source/gba/bios.s
@@ -1,21 +1,23 @@
-	.section .text
 	.thumb
 
-	.align 2
+	.balign 2
+	.section .text.IntrWait
 	.global IntrWait
 	.thumb_func
 IntrWait:
 	swi	0x04
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.VBlankIntrWait
 	.global VBlankIntrWait
 	.thumb_func
 VBlankIntrWait:
 	swi	0x05
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.Div
 	.global Div
 	.thumb_func
 Div:
@@ -27,85 +29,96 @@ Div:
 	stmia	r2!, {r0, r1, r3}
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.Sqrt
 	.global Sqrt
 	.thumb_func
 Sqrt:
 	swi	0x08
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.CpuSet
 	.global CpuSet
 	.thumb_func
 CpuSet:
 	swi	0x0B
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.CpuFastSet
 	.global CpuFastSet
 	.thumb_func
 CpuFastSet:
 	swi	0x0C
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.BitUnPack
 	.global BitUnPack
 	.thumb_func
 BitUnPack:
 	swi	0x10
 	bx	lr
 
-	.section .text.lz
-	.align 2
+	.balign 2
+	.section .text.LZ77UnCompWram
 	.global LZ77UnCompWram
 	.thumb_func
 LZ77UnCompWram:
 	swi	0x11
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.LZ77UnCompVram
 	.global LZ77UnCompVram
 	.thumb_func
 LZ77UnCompVram:
 	swi	0x12
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.HuffUnComp
 	.global HuffUnComp
 	.thumb_func
 HuffUnComp:
 	swi	0x13
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.RLUnCompWram
 	.global RLUnCompWram
 	.thumb_func
 RLUnCompWram:
 	swi	0x14
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.RLUnCompVram
 	.global RLUnCompVram
 	.thumb_func
 RLUnCompVram:
 	swi	0x15
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.Diff8UnFilterWram
 	.global Diff8UnFilterWram
 	.thumb_func
 Diff8UnFilterWram:
 	swi	0x16
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.Diff8UnFilterVram
 	.global Diff8UnFilterVram
 	.thumb_func
 Diff8UnFilterVram:
 	swi	0x17
 	bx	lr
 
-	.align 2
+	.balign 2
+	.section .text.Diff16UnFilter
 	.global Diff16UnFilter
 	.thumb_func
 Diff16UnFilter:
