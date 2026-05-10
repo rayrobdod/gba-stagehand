ca79343614315d0167cf50944a0463c57a3afbc6
Raymond Dodge <git@rayrobdod.name>
2026-05-10 02:51:26 -0400

    Potentially remove a string copy by changing TEST_FAIL's parameter to non-owning

--- tools/gfxc/test/_shared.hpp
+++ tools/gfxc/test/_shared.hpp
@@ -4,7 +4,7 @@
 
 void TEST_ASSERT_EQUAL_VECTOR_HEX8(std::vector<uint8_t> expected, std::vector<uint8_t> actual);
 void TEST_ASSERT_EQUAL_UNSIGNED(unsigned expected, unsigned actual);
-void TEST_FAIL(std::string reason);
+void TEST_FAIL(std::string_view reason);
 
 extern unsigned total;
 extern unsigned failed;
